574aee71cccf962ef70b22928c028b59d490b7f2
Johannes Eschricht <johannes@nortic.se>
2024-12-05 16:48:08 +0100

    fix: Revert types

--- src/json.ts
+++ src/json.ts
@@ -1,4 +1,5 @@
-import type { IconifyJSON } from '@iconify/types'
-import _json from '../.generated/json/nortic.json'
+import json from '../.generated/json/nortic.json'
 
-export const json: IconifyJSON = _json
+export {
+  json,
+}
